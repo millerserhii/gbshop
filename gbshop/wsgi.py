4cfa8fee5867e6033ce91be599dd83ec53ce42a9
Serhii Miller <48553674+millerserhii@users.noreply.github.com>
2022-01-04 17:40:28 +0200

    Lesson 3 (#3)
    * edited gitignore
    
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson 3. Models in context
    
    * Lesson 3. Urls for app
    
    * Lesson 3. Code style fix

--- gbshop/wsgi.py
+++ gbshop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbshop.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gbshop.settings")
 
 application = get_wsgi_application()
