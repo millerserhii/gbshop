ec591083b3d24be9980af56e90b6c3b9e20e63d4
Serhii Miller <48553674+millerserhii@users.noreply.github.com>
2022-01-26 17:14:06 +0200

    Lesson 4 (#4)
    * Lesson 4. Data from files
    
    * code cleanup
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD

--- gbshop/settings.py
+++ gbshop/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2tqv3^llvf*f8kx(hf$t436zj37@mlkgia7ffhk)8e%_79hucc"
+SECRET_KEY = (
+    "django-insecure-2tqv3^llvf*f8kx(hf$t436zj37@mlkgia7ffhk)8e%_79hucc"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
+    "authnapp",
 ]
+
+# Auth model
+AUTH_USER_MODEL = "authnapp.ShopUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
